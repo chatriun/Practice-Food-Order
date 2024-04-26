735a5a55aaa410be12449cb14d9dd2dfc4b0faba
salysaly <anyawee.sr@gmail.com>
2024-04-26 20:19:31 +0700

    <Modal />, <Cart />, open/close Cart

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,14 +2,20 @@ import { useContext } from "react";
 import logo from "../../public/logo.jpg";
 import Button from "../component/UI/Button";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
 const Header = () => {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const cartItems = cartCtx.items.reduce(
     (total, items) => total + items.quantity,
     0
   );
+
+  const handleShowCartModal = () => {
+    userProgressCtx.showCart();
+  };
   return (
     <header>
       <div id="main-header">
@@ -18,7 +24,9 @@ const Header = () => {
           <h1>reactfood</h1>
         </div>
         <nav>
-          <Button textOnly>cart ({cartItems})</Button>
+          <Button textOnly onClick={handleShowCartModal}>
+            cart ({cartItems})
+          </Button>
         </nav>
       </div>
     </header>
